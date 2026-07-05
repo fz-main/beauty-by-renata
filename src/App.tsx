4640564ea7ea6f1c0dfbaad3a40922d332ce9650
fz-main <velowebfz@proton.me>
2026-07-05 23:48:12 +0300

    fix: use HashRouter for reliable GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { AdminPage } from './pages/admin';
 import MainPage from './MainPage';
 
 function App() {
   return (
-    <BrowserRouter basename="/beauty-by-renata">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
